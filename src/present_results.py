4feff26a1d1af04cf407ec23ccf8b30f6f9ebf71
Tina Smirnova <tinanight98@gmail.com>
2020-05-26 16:20:49 +0300

    graphs

--- src/present_results.py
+++ src/present_results.py
@@ -21,27 +21,37 @@ def plot_predicted(mean, variance, real, dataset_name):
     plt.clf()
 
 
-def present_incubment(dir_name, dataset_name):
-    (incumbents, x_opt, f_opt, incumbent_values, runtime, overhead, X, y) = read_full_result(dir_name + dataset_name + '/run-0/')
+def present_incubment(dir_name, dataset_name, info_file):
+    (x_opt, f_opt, incubments, incumbent_values, runtime, overhead, X, y) = read_full_result(dir_name + dataset_name + '/run-0/')
 
     plt.plot(incumbent_values, linestyle='solid', color='blue', label='incubment')
+
+    ymin = min(incumbent_values)
+    xpos = incumbent_values.index(ymin)
+    xmin = xpos
+
+    info_file.write(dataset_name + ',' + str(ymin) + ',' + str(xmin) + '\n')
+
+    plt.plot(xmin, ymin, 'rx',  markersize=10, markeredgewidth=2, label='best: '+str(ymin))
     plt.xlabel('Iteration')
     plt.ylabel('1 - F-score')
     plt.legend()
     plt.title(dataset_name)
-    # time = datetime.datetime.now().strftime('-%H-%M-%S')
     plt.savefig('../png/incubment-iteration/' + dataset_name + '.png')
-    plt.show()
+    # plt.show()
     plt.clf()
 
 
 def present_all_incubments():
-    datasets = get_datasets_list('../datasets/')
+    # datasets = get_datasets_list('../datasets/')
+
+    info = open('../incubment-results.csv', 'w')
+    info.write('"name","best_incubment","achieved_iteration"\n')
+
+    datasets = ['page-blocks', 'robot-failures-lp1', 'mfeat-fourier', 'jungle_chess_2pcs_raw_endgame_complete', 'heart-switzerland', 'gas-drift-different-concentrations', 'wall-robot-navigation', 'jungle_chess_2pcs_endgame_panther_elephant', 'leaf', 'PopularKids', 'mfeat-karhunen', 'diggle_table_a2', 'rmftsa_sleepdata', 'semeion', 'desharnais', 'teachingAssistant', 'collins', 'volcanoes-a3', 'artificial-characters', 'volcanoes-a4', 'glass', 'nursery', 'shuttle', 'segment', 'heart-long-beach', 'vertebra-column', 'cnae-9', 'jannis', 'wine-quality-white', 'vehicle', 'ecoli', 'eye_movements', 'seeds', 'car', 'fabert', 'breast-tissue', 'thyroid-allbp', 'gas-drift', 'mfeat-factors', 'volcanoes-d1', 'har', 'satimage', 'Fashion-MNIST', 'seismic-bumps', 'pokerhand', 'helena', 'thyroid-allhyper', 'wine', 'balance-scale', 'microaggregation2', 'steel-plates-fault', 'tae', 'mfeat-pixel', 'gina_prior2', 'synthetic_control', 'cmc', 'energy-efficiency', 'iris', 'fars', 'abalone', 'prnn_viruses', 'Indian_pines', 'covertype', 'JapaneseVowels', 'user-knowledge', 'spectrometer', 'hayes-roth', 'robot-failures-lp5', 'prnn_fglass', 'waveform-5000', 'zoo', 'cardiotocography', 'mfeat-morphological', 'volcanoes-a1', 'tamilnadu-electricity', 'LED-display-domain-7digit']
     for dataset in datasets:
-        present_incubment('../optimization_results/f-score/random-log_ei-gp/', dataset)
+        present_incubment('../optimization_results/f-score/random-log_ei-gp/', dataset, info)
 
 
-# present_all_incubments()
-present_incubment('../optimization_results/f-score/random-log_ei-gp/', 'PopularKids')
-# present_result('optimization_results/differential_evolution-ei-gp/',
-#                'page-blocks')
+present_all_incubments()
+# present_incubment('../optimization_results/f-score/random-log_ei-gp/', 'PopularKids')
